ca2ca98afe605ab20a881fc923d14bef77959d71
Kevin Guerrero <kevin.guerrero@newshore.es>
2019-02-27 18:45:48 -0500

    add diagram and app

---
+++ PokemonElementos.java
@@ -0,0 +1,17 @@
+
+package pokemonelementos;
+
+
+public class PokemonElementos {
+
+   
+    public static void main(String[] args) {
+        Pokemon misPokemon[] = new Pokemon [7];
+        
+        
+        misPokemon[0] = new Pokemon("shooks","Fuego",10,"rojito");
+        
+        
+    }
+    
+}
